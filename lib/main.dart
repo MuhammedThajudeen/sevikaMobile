19aa946e7539fcd9959a7d1ebd7873ffec02ce26
MuhammedThajudeen <thajudeen533@gmail.com>
2024-01-14 16:50:52 +0530

    typos

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:sevika/provider/carousel_provider.dart';
-import 'package:sevika/provider/services_provider.dart';
-import 'package:sevika/screens/homescreen.dart';
 import 'package:provider/provider.dart';
+import 'package:sevika/view/homescreen.dart';
+import 'package:sevika/viewmodel/carousel_provider.dart';
+import 'package:sevika/viewmodel/services_provider.dart';
 void main() {
   runApp(const MyApp());
 }
